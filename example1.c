91f8d6f1d0f04e6830c3410d74c480f21430482a
YaaaHuuu <794674061@qq.com>
2015-09-13 16:49:03 -0400

    example

---
+++ example1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void function(int a, int b, int c) {
+   char buffer1[5];
+   char buffer2[10];
+}
+
+void main() {
+  function(1,2,3);
+}
